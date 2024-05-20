217e83d005bdfa1a4848e7c21405df1ae3ffa725
Django-Argus <malaska@mailo.com>
2024-05-20 17:30:53 +0200

    Update build

--- src/net/argus/timetable/MainTimeTable.java
+++ src/net/argus/timetable/MainTimeTable.java
@@ -5,7 +5,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
